a6fb9584f0d273845cfccdaf83ce57a22604c7e2
Daniel Joachim Larsen <dal@monsido.com>
2020-06-02 09:23:15 +0200

    use directly exported member from angular-animate dep

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,13 @@
 import angular from 'angular';
-import 'angular-animate';
-// Impost global styles int the main js file
+import ngAnimate from 'angular-animate';
+// Import global styles int the main js file
 import '../style/app.scss';
 
 // Modules
 import PuzzleModule from './components/memory/memory.module';
 
 angular.module('app', [
-  'ngAnimate',
+  ngAnimate,
   PuzzleModule
 ])
   .component('app', {
